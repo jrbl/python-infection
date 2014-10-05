4e5020e701d43a93a89cc64ff31246c2f9512cea
Joe Blaylock <jrbl@jrbl.org>
2014-10-04 21:19:18 -0700

    Working unit tests and bugfixes

--- infection.py
+++ infection.py
@@ -59,12 +59,17 @@ def limited_infection(start_user, max_infections=0):
     * max_infections is the target number of users we'd like to be infected. if max_infections 
       is 0, return total_infection(start_user) instead.
     """
-    to_infect = {start_user}
+    to_infect = set([start_user])
     infected_set = set()
     if max_infections == 0:
         return total_infection(start_user=start_user)
     while len(infected_set) < max_infections:
-        user = to_infect.pop()
+        try:
+            user = to_infect.pop()
+        except KeyError:
+            # pop from empty set means our population is smaller than max_infections
+            # so just return whatever we've calculated so far
+            return infected_set
         infected_set.add(user)
         def keyfunc(coach):
             # If we have many coaches, pick the one with the most infected students
@@ -89,17 +94,15 @@ def total_infection(start_user=None, population=None):
     to_infect = set()
     infected_set = set()
     if start_user is None and population is None:
-        raise ValueError, "Both start_user and population may not be unspecified."
+        raise TypeError, "Both start_user and population may not be unspecified."
     if start_user and not population:
         to_infect.update({start_user})
     if population and not start_user:
         to_infect.update(population.infected)
     while len(to_infect) > 0:
         user = to_infect.pop()
-        print "popped {}".format(user)
         infected_set.add(user)
         add_infections = user.coached_by.union(user.coaching) - to_infect - infected_set
-        print "enlisting {}".format(add_infections)
         to_infect.update(add_infections)
     return infected_set
 
--- infection_test.py
+++ infection_test.py
@@ -1,33 +1,157 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Test Cases for limited_infection() and total_infection()
 
+See SPEC.txt for details.
+"""
 
 import unittest
 
+from infection import limited_infection, total_infection, User
 
-# Testing goal: A, B, and C are the only users and they share no coaching
-# relationship. total_infection(A) results in only A becoming infected.
-# limited_infection(A, 3) results in all three becoming infected.
-#
-# A coaches B coaches C. total_infection(A), total_infection(B), and
-# total_infection(C) each result in everyone becoming infected.
-# limited_infection(A, 2) results in A and B becoming infected.
 
-class TestLimitedInfection(unittest.TestCase):
-    pass
+class LimitedInfectionTestCase(unittest.TestCase):
+    """Test the various calling conventions of limited_infection()
 
-class TestTotalInfection(unittest.TestCase):
-    pass
+    None of these tests check when max_infections=0 because those checks
+    are in TotalInfectionTestCase, below.
+    """
+    def test_no_user(self):
+        """It is an error to call limited_infection without a user object."""
+        with self.assertRaises(TypeError):
+            limited_infection()
 
-def limited_infection():
-    pass
+    def test_single_user(self):
+        """Limited infection of a single user infects that user as expected."""
+        A = User()
+        infected = limited_infection(A, 1)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(A, 1e7)
+        self.assertEqual(set([A]), infected)
 
-def total_infection():
-    pass
+    def test_two_users_with_relation(self):
+        """Limited infection of two users behaves well with different limit requests"""
+        A = User()
+        B = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        infected = limited_infection(A, 1)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(A, 2)
+        self.assertEqual(set([A, B]), infected)
+        infected = limited_infection(A, 1e7)
+        self.assertEqual(set([A, B]), infected)
+        
+    def test_two_users_no_relation(self):
+        """Limited infection of two independent users treats them independently"""
+        A = User()
+        B = User()
+        infected = limited_infection(A, 1)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(A, 2)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(B, 1e7)
+        self.assertEqual(set([B]), infected)
 
-def main():
-    pass
+    def test_two_users_cyclic(self):
+        """Cyclic users still behave as expected."""
+        A = User(); B = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        A.coached_by.add(B); B.coaching.add(A)
+        infected = limited_infection(A, 1)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(A, 2)
+        self.assertEqual(set([A, B]), infected)
+        infected = limited_infection(A, 1e7)
+        self.assertEqual(set([A, B]), infected)
+        
+    def test_three_users(self):
+        """Limited infection of three users behaves well with different limit requests"""
+        A = User(); B = User(); C = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        B.coaching.add(C); C.coached_by.add(B)
+        infected = limited_infection(A, 1)
+        self.assertEqual(set([A]), infected)
+        infected = limited_infection(A, 2)
+        self.assertEqual(set([A, B]), infected)
+        infected = limited_infection(B, 2)
+        self.assertEqual(set([C, B]), infected)
+        infected = limited_infection(C, 2)
+        self.assertEqual(set([C, B]), infected)
+        infected = limited_infection(A, 1e7)
+        self.assertEqual(set([A, B, C]), infected)
+
+
+class TotalInfectionTestCase(unittest.TestCase):
+    # TODO: test total infection when calling limited infection here
+
+    def test_no_user(self):
+        """It is an error to call limited_infection without a user object."""
+        with self.assertRaises(TypeError):
+            total_infection()
+
+    def test_single_user(self):
+        """Total infection of a single user infects that user as expected."""
+        A = User()
+        infected = limited_infection(A)
+        self.assertEqual(set([A]), infected)
+
+    def test_two_users_with_relation(self):
+        """Total infection of two users infects them both regardless of relationship"""
+        A = User(); B = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        infected = total_infection(A)
+        self.assertEqual(set([A, B]), infected)
+        A.coached_by.add(B); B.coaching.add(A)
+        infected = total_infection(A)
+        self.assertEqual(set([A, B]), infected)
+        A.coaching.clear(); B.coached_by.clear()
+        infected = total_infection(A)
+        self.assertEqual(set([A, B]), infected)
+        
+    def test_two_users_no_relation(self):
+        """Total infection of two independent users treats them independently"""
+        A = User(); B = User()
+        infected = total_infection(A)
+        self.assertEqual(set([A]), infected)
+        infected = total_infection(B)
+        self.assertEqual(set([B]), infected)
+
+    def test_three_users(self):
+        """Total infection of three users behaves well with different limit requests"""
+        A = User(); B = User(); C = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        infected = total_infection(A)
+        self.assertEqual(set([A, B]), infected)
+        infected = total_infection(C)
+        self.assertEqual(set([C]), infected)
+        B.coaching.add(C); C.coached_by.add(B)
+        infected = total_infection(A)
+        self.assertEqual(set([A, B, C]), infected)
+        infected = total_infection(C)
+        self.assertEqual(set([A, B, C]), infected)
+
+    def test_limited_single_user_count0(self):
+        """Limited infection of count 0 with single user infects that user as expected."""
+        A = User()
+        infected = limited_infection(A, 0)
+        self.assertEqual(set([A]), infected)
+
+    def test_limited_two_users_count0(self):
+        """Limited infection of count 0 with two users infects them both"""
+        A = User()
+        B = User()
+        A.coaching.add(B); B.coaching.add(A)
+        infected = limited_infection(A, 0)
+        self.assertEqual(set([A, B]), infected)
+        
+    def test_limited_three_users_count0(self):
+        """Limited infection of count 0 with three users infects everyone"""
+        A = User(); B = User(); C = User()
+        A.coaching.add(B); B.coached_by.add(A)
+        B.coaching.add(C); C.coached_by.add(B)
+        infected = limited_infection(B, 0)
+        self.assertEqual(set([A, B, C]), infected)
 
 
 if __name__ == "__main__":
-    main()
+    unittest.main()
